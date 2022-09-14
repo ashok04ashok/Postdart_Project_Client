03f952dce78f629f0701abcf7cc9fed47d0649b3
Ashok M <102283057+ashok04ashok@users.noreply.github.com>
2022-09-14 13:08:42 +0530

    Update Api.js

--- src/global/Api.js
+++ src/global/Api.js
@@ -1,5 +1,5 @@
 // API
-// export const PRODUCT_API = "https://postdart.herokuapp.com/api"
-export const PRODUCT_API = "http://localhost:8000/api"
+export const PRODUCT_API = "https://postdart.herokuapp.com/api"
+// export const PRODUCT_API = "http://localhost:8000/api"
 
 
